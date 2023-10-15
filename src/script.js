a3c2fe069b007cc71078ce31ba63231f9b30697e
Ujjawal Sharma <82134419+UjjawalSharma05@users.noreply.github.com>
2023-10-15 21:00:10 +0530

    Pre-release V1.1

--- src/script.js
+++ src/script.js
@@ -1,9 +1,22 @@
 var mobMenuContainer = document.getElementById("mobMenuCont");
 var mobMenuWrap = document.getElementById("mobMenuWrap");
+var button = document.getElementById("btn");
+var menuon = 0;
 
-function mobMenuToggle() {    
-    mobMenuContainer.classList.add("displayon");
-    mobMenuWrap.classList.add("displayon");
+function mobMenuToggle() {
+    if (menuon == 0) {
+        mobMenuContainer.classList.add("displayon");
+        mobMenuWrap.classList.add("displayon");
+        button.classList.add("close");
+        menuon = 1;
+    } else {
+        mobMenuContainer.classList.remove("displayon");
+        mobMenuWrap.classList.remove("displayon");
+        menuon = 0;
+    }
+    //mobMenuContainer.classList.add("displayon");
+    //mobMenuWrap.classList.add("displayon");
+
 }
 
 function closeMobMenuToggle() {
