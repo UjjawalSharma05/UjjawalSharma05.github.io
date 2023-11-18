ae84e2dac77be75acbd49e6fb753023c9c5ea1d5
UjjawalSharma05 <82134419+UjjawalSharma05@users.noreply.github.com>
2023-11-18 15:48:09 +0530

    17 Nov 2023
    index completed.
    template completed.
    404 page completed.
    deploying for mobile testing.

--- src/master.js
+++ src/master.js
@@ -1,15 +1,15 @@
-function menuToggle()
+function menuToggle() //menuToggle function to be called when menu button is clicked
 {
-    if(document.getElementById("link-menu").classList.contains('enable'))
+    if(document.getElementById("link-menu").classList.contains('enable')) //if links menu is open
     {
-        document.getElementById("link-menu").classList.remove('enable');
-        document.getElementById("btn").classList.remove('enable');
-        document.getElementById("icon").innerHTML ="menu";
+        document.getElementById("link-menu").classList.remove('enable'); //close links menu
+        document.getElementById("btn").classList.remove('enable'); //remove the highlight from the button
+        document.getElementById("icon").innerHTML ="menu"; //change the close icon to menu icon
     }
     else
     {
-        document.getElementById("link-menu").classList.add('enable');
-        document.getElementById("btn").classList.add('enable');
-        document.getElementById("icon").innerHTML ="close";
+        document.getElementById("link-menu").classList.add('enable'); //open links menu
+        document.getElementById("btn").classList.add('enable'); //add highlight to the button
+        document.getElementById("icon").innerHTML ="close"; //change the menu icon to close icon
     }
 }
